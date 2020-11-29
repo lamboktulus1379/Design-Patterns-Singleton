a66c5fffd88602640bebc2bd71b4817b8db0c1d3
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-11-29 16:03:19 +0700

    Add project files.

---
+++ Design Patterns – Singleton/ISingletonContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns___Singleton
+{
+    public interface ISingletonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
---
+++ Design Patterns – Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Design_Patterns___Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            Console.WriteLine(db.GetPopulation("Washington, D.C."));
+            var db2 = SingletonDataContainer.Instance;
+            Console.WriteLine(db2.GetPopulation("London"));
+            var db3 = SingletonDataContainer.Instance;
+            var db4 = SingletonDataContainer.Instance;
+        }
+    }
+}
